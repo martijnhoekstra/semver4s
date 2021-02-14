bde37f26fd82740079b4a0ac9e9e9f5ead28c165
Martijn Hoekstra <martijnhoekstra@gmail.com>
2021-02-14 12:34:35 +0100

    clean up matchers
    add documentation, limit visibility, implement upper bound

--- semver4s/src/main/scala/semver4s/Matchers.scala
+++ semver4s/src/main/scala/semver4s/Matchers.scala
@@ -2,11 +2,8 @@ package semver4s
 
 import cats.syntax.all._
 
-//import cats.kernel.Order
-//import cats.kernel.Comparison._
 import cats.data.NonEmptyList
 import semver4s.PreReleaseBehaviour._
-import cats.kernel.Comparison._
 import cats.kernel.Order
 
 sealed trait PreReleaseBehaviour
@@ -24,12 +21,30 @@ object PreReleaseBehaviour {
   case object Strict extends PreReleaseBehaviour
 }
 
+/** A matcher that can test whether a version matches the matcher.
+  *
+  * The NPM matchers are
+  *
+  * - Primitive matchers >, <, >=, <= or = with a (potentially partial) version,
+  *   e.g. `<1.3` or `>=1.3.4-pre.1`.
+  * - Hyphen ranges, e.g. 1.3 - 2
+  * - Tilde ranges: e.g. ~1.3.2 flexible patch.
+  * - Caret ranges: e.g. ^1.3.2 flexible minor and patch.
+  * - Wildcard ranges: e.g. 1.3 / 1.3.* / 1.3.x
+  * - Conjunction (AND) ranges: e.g. >1.3 <2.3.1
+  * - Disjunction (OR) ranges: e.g. any version in three any version in 1.2: 1.2 3 || 1.2.*
+  */
 sealed trait Matcher extends Product with Serializable {
   def matches(that: Version): Boolean
   def matches(that: Version, pre: PreReleaseBehaviour): Boolean
 }
 
+/** Simple matchers: all matcher except conjunction and disjunction (AND and OR) ranges
+  */
 sealed trait Simple extends Matcher
+
+/** Hyphen ranges, ranges bounded by a lower and upper bound
+  */
 case class Hyphen(lower: Partial, upper: Partial) extends Simple {
   def matches(version: Version): Boolean = matches(version, Strict)
   def matches(version: Version, pre: PreReleaseBehaviour): Boolean = {
@@ -43,34 +58,14 @@ case class Hyphen(lower: Partial, upper: Partial) extends Simple {
       case Patch(major, minor, patch) =>
         (major, minor, patch) >= ((version.major, version.minor, version.patch))
       case Pre(major, minor, patch, _) =>
-        (major, minor, patch) > (
-          (
-            version.major,
-            version.minor,
-            version.patch
-          )
-        ) //Some(pre)) >= ((version.major, version.minor, version.pre))
+        (major, minor, patch) > ((version.major, version.minor, version.patch))
     }
     (lowerVersion <= version) && matchesUpper
   }
-  /*
-    val matchLower = Matcher.gte(lower match {
-      case Wild                          => Version(0, 0, 0)
-      case Major(major)                  => Version(major, 0, 0)
-      case Minor(major, minor)           => Version(major, minor, 0)
-      case Patch(major, minor, patch)    => Version(major, minor, patch)
-      case Pre(major, minor, patch, pre) => Version(major, minor, patch, Some(pre), None)
-    })
-    upper match {
-      case Wild                => matchLower
-      case Major(major)        => matchLower && LT(Version(major + 1, 0, 0))
-      case Minor(major, minor) => matchLower && LT(Version(major, minor + 1, 0))
-      case Patch(major, minor, patch) =>
-        matchLower && Matcher.lte(Version(major, minor, patch))
-      case Pre(major, minor, patch, pre) =>
-        matchLower && Matcher.lte(Version(major, minor, patch, Some(pre), None)) // <- ???
-    } */
 }
+
+/** Caret ranges, flexible minor and patch
+  */
 case class Caret(p: Partial) extends Simple {
   def matches(that: Version): Boolean = matches(that, Strict)
   def matches(that: Version, preBehaviour: PreReleaseBehaviour): Boolean = {
@@ -90,6 +85,9 @@ case class Caret(p: Partial) extends Simple {
     }
   }
 }
+
+/** Tilde ranges: Flexible patch
+  */
 case class Tilde(p: Partial) extends Simple {
   def matches(that: Version): Boolean = matches(that, Strict)
   def matches(that: Version, preBehaviour: PreReleaseBehaviour): Boolean = {
@@ -107,6 +105,12 @@ case class Tilde(p: Partial) extends Simple {
     }
   }
 }
+
+/** Conjunction: Matches if all simple constituents match
+  *
+  * For a strict match all constituents must match loosely, and any of them
+  * strictly with regards to pre-release behaviour.
+  */
 case class And(simples: NonEmptyList[Simple]) extends Matcher {
   def matches(that: Version): Boolean = matches(that, Strict)
   def matches(that: Version, preBehaviour: PreReleaseBehaviour): Boolean = {
@@ -116,6 +120,9 @@ case class And(simples: NonEmptyList[Simple]) extends Matcher {
     simples.map(s => s.matches(that, preBehaviour)).reduceLeft(_ || _)
   }
 }
+
+/** Disjunction: Matches if any of the constituents match
+  */
 case class Or(ands: NonEmptyList[And]) extends Matcher {
   def matches(that: Version): Boolean = matches(that, Strict)
   def matches(that: Version, preBehaviour: PreReleaseBehaviour): Boolean = {
@@ -138,7 +145,6 @@ case class Exact(p: Partial) extends Primitive {
       coreMatches && preMatches
     case Wild => true
   }
-
 }
 
 case class GT(p: Partial) extends Primitive {
@@ -265,11 +271,26 @@ case class LTE(p: Partial) extends Primitive {
 }
 
 object Matcher {
+  implicit private[this] val precedence: Order[Version] = Version.precedence
 
-  def gt(p: Partial)  = GT(p)
+  /** a matches that matches if the given version is greater than this partial
+    */
+  def gt(p: Partial) = GT(p)
+
+  /** a matcher that matches if the given version is greater or equal to this partial
+    */
   def gte(p: Partial) = GTE(p)
-  def lt(p: Partial)  = LT(p)
+
+  /** a matcher that matches if the given version is less than this partial
+    */
+  def lt(p: Partial) = LT(p)
+
+  /** a matcher that matches if the given version is less than or equal to this partial
+    */
   def lte(p: Partial) = LTE(p)
+
+  /** a matcher that matches if the given version is equal to this partial
+    */
   def eqv(p: Partial) = Exact(p)
 
   implicit final class SimpleAndOps(val self: Simple) extends AnyVal {
@@ -293,56 +314,91 @@ object Matcher {
     }
   }
 
+  /** The lower bound of the given matcher
+    */
   def lowerBound(m: Matcher): Bound = m match {
     case Hyphen(lower, _) => lowerBound(GT(lower))
     case Caret(p)         => Inclusive(p.version)
     case Tilde(p)         => Inclusive(p.version)
-    case And(simples) =>
-      simples
-        .map(lowerBound)
-        .reduceLeft((a, b) =>
-          ((a, b): @unchecked) match {
-            case (Inclusive(v1), Inclusive(v2)) => Inclusive(Version.precedence.max(v1, v2))
-            case (Exclusive(v1), Exclusive(v2)) => Exclusive(Version.precedence.max(v1, v2))
-            case (i @ Inclusive(v1), e @ Exclusive(v2)) =>
-              Version.precedence.comparison(v1, v2) match {
-                case GreaterThan => i
-                case _           => e
-              }
-            case (e @ Exclusive(v1), i @ Inclusive(v2)) =>
-              Version.precedence.comparison(v1, v2) match {
-                case LessThan => i
-                case _        => e
-              }
-          }
-        )
-    case Or(ands) =>
-      ands
-        .map(lowerBound)
-        .reduceLeft((a, b) =>
-          ((a, b): @unchecked) match {
-            case (Inclusive(v1), Inclusive(v2)) => Inclusive(Version.precedence.min(v1, v2))
-            case (Exclusive(v1), Exclusive(v2)) => Exclusive(Version.precedence.min(v1, v2))
-            case (i @ Inclusive(v1), e @ Exclusive(v2)) =>
-              Version.precedence.comparison(v1, v2) match {
-                case LessThan => i
-                case _        => e
-              }
-            case (e @ Exclusive(v1), i @ Inclusive(v2)) =>
-              Version.precedence.comparison(v1, v2) match {
-                case GreaterThan => i
-                case _           => e
-              }
-          }
-        )
-    case Exact(p)   => Inclusive(p.version)
-    case GT(p: Pre) => Exclusive(p.version)
-    case GT(p)      => Inclusive(p.increment.version)
-    case GTE(p)     => Inclusive(p.version)
-    case LT(_)      => Inclusive(Version(0, 0, 0))
-    case LTE(_)     => Inclusive(Version(0, 0, 0))
+    case And(simples)     => simples.map(lowerBound).reduceLeft(maxLower)
+    case Or(ands)         => ands.map(lowerBound).reduceLeft(minLower)
+    case Exact(p)         => Inclusive(p.version)
+    case GT(p: Pre)       => Exclusive(p.version)
+    case GT(p)            => Inclusive(p.increment.version)
+    case GTE(p)           => Inclusive(p.version)
+    case LT(_)            => Inclusive(Version(0, 0, 0))
+    case LTE(_)           => Inclusive(Version(0, 0, 0))
   }
 
-  def upperBound(m: Matcher): Bound = ???
+  /** The upper bound of the given matcher
+    */
+  def upperBound(m: Matcher): Bound = m match {
+    case Hyphen(_, upper) =>
+      upper match {
+        case Wild     => Unbounded
+        case m: Major => Exclusive(m.increment.version)
+        case m: Minor => Exclusive(m.increment.version)
+        case p: Patch => Inclusive(p.version)
+        case p: Pre   => Inclusive(p.version)
+      }
+    case Caret(p) =>
+      p match {
+        case Wild                => Unbounded //^* kinda weird, but whatever
+        case Major(m)            => Exclusive(Major(m + 1).version)
+        case Minor(major, minor) => Exclusive(Minor(major, minor + 1).version)
+        case Patch(0, 0, _)      => Inclusive(p.version)
+        case Patch(0, minor, _)  => Exclusive(Minor(0, minor + 1).version)
+        case Patch(major, _, _)  => Exclusive(Major(major).version)
+        //^0.0.3-beta := >=0.0.3-beta <0.0.4 Note that prereleases in the 0.0.3 version only will be allowed, if they are greater than or equal to beta
+        case Pre(0, 0, pat, _) => Exclusive(Version(0, 0, pat + 1))
+        //^1.2.3-beta.2 := >=1.2.3-beta.2 <2.0.0 //1.2.3-beta.4 would be allowed, but 1.2.4-beta.2 would not
+        case Pre(0, min, _, _) => Exclusive(Minor(0, min + 1).version)
+        case Pre(maj, _, _, _) => Exclusive(Major(maj + 1).version)
+      }
+    case Tilde(p) =>
+      p match {
+        case Wild         => Unbounded                           //~* kinda weird, but whatever
+        case Major(major) => Exclusive(Major(major + 1).version) //same as caret
+        case Minor(major, minor) =>
+          Exclusive(Minor(major, minor + 1).version) //in the glorious future
+        case Patch(major, minor, _) =>
+          Exclusive(Minor(major, minor + 1).version) //binders should be
+        case Pre(major, minor, _, _) =>
+          Exclusive(Minor(major, minor + 1).version) //allowed in alternative
+      }
+    case Exact(p) => Exclusive(p.increment.version)
+    case GT(_)    => Unbounded
+    case GTE(_)   => Unbounded
+    case LT(p)    => Exclusive(p.version)
+    case LTE(p) =>
+      p match {
+        case Wild            => Unbounded
+        case Major(m)        => Exclusive(Version(m + 1, 0, 0))
+        case Minor(maj, min) => Exclusive(Version(maj, min + 1, 0))
+        case Patch(_, _, _)  => Inclusive(p.version)
+        case Pre(_, _, _, _) => Inclusive(p.version)
+      }
+    case And(simples) => simples.map(upperBound).reduceLeft(minUpper)
+    case Or(ands)     => ands.map(upperBound).reduceLeft(maxUpper)
+  }
+
+  private def maxUpper(b1: Bound, b2: Bound): Bound = (b1, b2) match {
+    case (Unbounded, _)                              => Unbounded
+    case (_, Unbounded)                              => Unbounded
+    case (b1: Bounded, b2: Bounded) if b1.by > b2.by => b1
+    case (b1: Bounded, b2: Bounded) if b2.by > b1.by => b2
+    case (i: Inclusive, _)                           => i
+    case (_, o)                                      => o
+  }
+  private def maxLower(b1: Bound, b2: Bound): Bound = (b1, b2) match {
+    case (Unbounded, x)                              => x
+    case (x, Unbounded)                              => x
+    case (b1: Bounded, b2: Bounded) if b1.by > b2.by => b1
+    case (b1: Bounded, b2: Bounded) if b2.by > b1.by => b2
+    case (i: Inclusive, _)                           => i
+    case (_, o)                                      => o
+  }
+  private def minUpper(b1: Bound, b2: Bound): Bound = if (maxUpper(b1, b2) == b1) b2 else b1
+  private def minLower(b1: Bound, b2: Bound): Bound = if (maxLower(b1, b2) == b1) b2 else b1
 
 }
