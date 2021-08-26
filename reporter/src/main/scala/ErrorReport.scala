c1f440460a3f1ed52d04e3569e59af0e49c59431
Martijn Hoekstra <martijnhoekstra@gmail.com>
2021-08-26 15:35:52 +0200

    format error report

--- reporter/src/main/scala/ErrorReport.scala
+++ reporter/src/main/scala/ErrorReport.scala
@@ -2,10 +2,8 @@ package catsparse.reporting
 
 import cats.data.NonEmptyList
 
-/**
-  * An error report, consisting of the context around which the error occurred,
-  * A pointer to the error positon and a list of formatted messages describing
-  * the failed expectations at that point
+/** An error report, consisting of the context around which the error occurred, A pointer to the
+  * error positon and a list of formatted messages describing the failed expectations at that point
   *
   * @param context
   * @param caretPosition
@@ -13,4 +11,8 @@ import cats.data.NonEmptyList
   */
 case class ErrorReport(context: String, caretPosition: Int, messages: NonEmptyList[String]) {
   def caretLine = " " * caretPosition + "^"
+  def messagesAsBlock(separator: String = "\n"): String = messages match {
+    case NonEmptyList(head, Nil) => head
+    case _ => messages.toList.mkString(s"any of the following:$separator", s"Or $separator", "")
+  }
 }
