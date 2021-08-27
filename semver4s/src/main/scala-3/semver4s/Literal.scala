94afd75e6d9db823fbd5fdfabb0caa67ee32d94b
Martijn Hoekstra <martijnhoekstra@gmail.com>
2021-08-27 11:02:19 +0200

    format scala-3 macros

--- semver4s/src/main/scala-3/semver4s/Literal.scala
+++ semver4s/src/main/scala-3/semver4s/Literal.scala
@@ -4,14 +4,16 @@ import scala.quoted.*
 import cats.data.NonEmptyList
 
 object Literal:
-  inline def version(inline s: String) = ${versionImpl('{s})}
-  inline def matcher(inline s: String) = ${matcherImpl('{s})}
+  inline def version(inline s: String) = ${ versionImpl('{ s }) }
+  inline def matcher(inline s: String) = ${ matcherImpl('{ s }) }
 
   extension (inline sc: StringContext)
-    inline def v(inline args: Any*) = ${interpolatedVersionImpl('{sc})}
-    inline def m(inline args: Any*) = ${interpolatedMatcherImpl('{sc})}
+    inline def v(inline args: Any*) = ${ interpolatedVersionImpl('{ sc }) }
+    inline def m(inline args: Any*) = ${ interpolatedMatcherImpl('{ sc }) }
 
-  private def unliftLiteral[A: FromExpr, B: ToExpr](a: Expr[A], f: A => Option[B])(using Quotes): Expr[B] =
+  private def unliftLiteral[A: FromExpr, B: ToExpr](a: Expr[A], f: A => Option[B])(using
+      Quotes
+  ): Expr[B] =
     Expr(f(a.valueOrError).getOrElse {
       quotes.reflect.report.error("value not in unlifted domain of f")
       ???
@@ -20,48 +22,57 @@ object Literal:
   given ToExpr[Version] with
     def apply(x: Version)(using Quotes): Expr[Version] = x match
       case Version(maj, min, pat, pre, bld) =>
-        '{Version.unsafe(${Expr(maj)}, ${Expr(min)}, ${Expr(pat)}, ${Expr(pre)}, ${Expr(bld)})}
+        '{
+          Version.unsafe(
+            ${ Expr(maj) },
+            ${ Expr(min) },
+            ${ Expr(pat) },
+            ${ Expr(pre) },
+            ${ Expr(bld) }
+          )
+        }
 
   given ToExpr[Matcher.Simple] with
     import Matcher.*
     def apply(m: Simple)(using Quotes): Expr[Simple] = m match
-      case Hyphen(below, above) => '{Hyphen(${Expr(below)}, ${Expr(above)})}
-      case Caret(p) => '{Caret(${Expr(p)})}
-      case Tilde(p) => '{Tilde(${Expr(p)})}
-      case Exact(p) =>'{Exact(${Expr(p)})}
-      case GT(p) => '{GT(${Expr(p)})}
-      case GTE(p) => '{GTE(${Expr(p)})}
-      case LT(p) => '{LT(${Expr(p)})}
-      case LTE(p) => '{LTE(${Expr(p)})}
+      case Hyphen(below, above) => '{ Hyphen(${ Expr(below) }, ${ Expr(above) }) }
+      case Caret(p)             => '{ Caret(${ Expr(p) }) }
+      case Tilde(p)             => '{ Tilde(${ Expr(p) }) }
+      case Exact(p)             => '{ Exact(${ Expr(p) }) }
+      case GT(p)                => '{ GT(${ Expr(p) }) }
+      case GTE(p)               => '{ GTE(${ Expr(p) }) }
+      case LT(p)                => '{ LT(${ Expr(p) }) }
+      case LTE(p)               => '{ LTE(${ Expr(p) }) }
 
   given ToExpr[Matcher.And] with
     import Matcher.*
-    def apply(m: And)(using Quotes): Expr[And] = '{And(${Expr(m.simples)})}
+    def apply(m: And)(using Quotes): Expr[And] = '{ And(${ Expr(m.simples) }) }
 
   given ToExpr[Matcher.Or] with
     import Matcher.*
-    def apply(m: Or)(using Quotes): Expr[Or] = '{Or(${Expr(m.ands)})}
+    def apply(m: Or)(using Quotes): Expr[Or] = '{ Or(${ Expr(m.ands) }) }
 
   given ToExpr[Matcher] with
     import Matcher.*
     def apply(m: Matcher)(using Quotes): Expr[Matcher] = m match
       case s: Simple => Expr(s)
-      case o: Or => Expr(o)
-      case a: And => Expr(a)
-
+      case o: Or     => Expr(o)
+      case a: And    => Expr(a)
 
   given ToExpr[Partial] with
     import Partial.*
     def apply(x: Partial)(using Quotes): Expr[Partial] = x match
-      case Wild => '{Partial.Wild}
-      case Major(m) => '{Partial.unsafe(${Expr(m)})}
-      case Minor(maj, min) => '{Partial.unsafe(${Expr(maj)}, ${Expr(min)})}
-      case Patch(maj, min, pat) => '{Partial.unsafe(${Expr(maj)}, ${Expr(min)}, ${Expr(pat)})}
-      case Pre(maj, min, pat, pre) => '{Partial.unsafe(${Expr(maj)}, ${Expr(min)}, ${Expr(pat)}, ${Expr(pre)})}
+      case Wild            => '{ Partial.Wild }
+      case Major(m)        => '{ Partial.unsafe(${ Expr(m) }) }
+      case Minor(maj, min) => '{ Partial.unsafe(${ Expr(maj) }, ${ Expr(min) }) }
+      case Patch(maj, min, pat) =>
+        '{ Partial.unsafe(${ Expr(maj) }, ${ Expr(min) }, ${ Expr(pat) }) }
+      case Pre(maj, min, pat, pre) =>
+        '{ Partial.unsafe(${ Expr(maj) }, ${ Expr(min) }, ${ Expr(pat) }, ${ Expr(pre) }) }
 
-  given [T: ToExpr : Type]: ToExpr[NonEmptyList[T]] with
+  given [T: ToExpr: Type]: ToExpr[NonEmptyList[T]] with
     def apply(xs: NonEmptyList[T])(using Quotes) =
-      '{NonEmptyList(${Expr(xs.head)}, ${Expr(xs.tail)})}
+      '{ NonEmptyList(${ Expr(xs.head) }, ${ Expr(xs.tail) }) }
 
   private def versionImpl(expr: Expr[String])(using Quotes): Expr[Version] =
     unliftLiteral(expr, a => parsing.SemverParser.semver.parseAll(a).toOption)
@@ -74,7 +85,9 @@ object Literal:
     sc.parts.toList match
       case List(single) => versionImpl(Expr(single))
       case _ =>
-        quotes.reflect.report.error("version interpolator is not allowed to have any interpolated values")
+        quotes.reflect.report.error(
+          "version interpolator is not allowed to have any interpolated values"
+        )
         ???
   }
 
@@ -83,6 +96,8 @@ object Literal:
     sc.parts.toList match
       case List(single) => matcherImpl(Expr(single))
       case _ =>
-        quotes.reflect.report.error("matcher interpolator is not allowed to have any interpolated values")
+        quotes.reflect.report.error(
+          "matcher interpolator is not allowed to have any interpolated values"
+        )
         ???
   }
